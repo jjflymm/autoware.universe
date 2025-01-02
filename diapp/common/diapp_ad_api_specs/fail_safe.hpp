65f402685539cfb68651a0269f5dcac5e69b79ae
Terrell Franzman <52446895+jjflymm@users.noreply.github.com>
2025-01-03 05:12:15 +0800

    需要在 ROS 2 系统中处理 MRM 状态消息的场景，特别是在需要确保消息可靠传输和持久化的情况下。
    定义与 MRM 状态消息相关的元数据，包括消息类型、主题名称、队列深度、可靠性策略和持久性策略。
    
    提供标准化的接口，方便在 ROS 2 系统中发布和订阅 MRM 状态消息。

---
+++ diapp/common/diapp_ad_api_specs/fail_safe.hpp
@@ -0,0 +1,36 @@
+// Copyright 2022 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DIAPP_AD_API_SPECS__FAIL_SAFE_HPP_
+#define DIAPP_AD_API_SPECS__FAIL_SAFE_HPP_
+
+#include <rclcpp/qos.hpp>
+
+#include <diapp_adapi_msgs/msg/mrm_state.hpp>
+
+namespace diapp_ad_api::fail_safe
+{
+
+struct MrmState
+{
+  using Message = diapp_adapi_msgs::msg::MrmState;
+  static constexpr char name[] = "/api/fail_safe/mrm_state";
+  static constexpr size_t depth = 1;
+  static constexpr auto reliability = RMW_QOS_POLICY_RELIABILITY_RELIABLE;
+  static constexpr auto durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL;
+};
+
+}  // namespace diapp_ad_api::fail_safe
+
+#endif  // DIAPP_AD_API_SPECS__FAIL_SAFE_HPP_
